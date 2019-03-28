3b564ea188a4af657dc3bd8cc468c6ce5384cf9e
fariaswag@gmail.com <G7y9ej2n>
2019-03-28 20:53:43 -0300

    removendo

--- desing-pattens-1/src/main/java/interfaces/IImposto.kt
+++
@@ -1,8 +0,0 @@
-package main.java.interfaces
-
-import main.java.modelo.Orcamento
-
-interface IImposto {
-
-    fun calcula(orcamento: Orcamento): Double
-}
--- desing-pattens-1/src/main/java/modelo/Item.kt
+++
@@ -1,3 +0,0 @@
-package main.java.modelo
-
-class Item(val nome: String)
